86fe5e6c761fcb9cb2d7cfde255460dd6327c498
daknine10 <156335553+daknine10@users.noreply.github.com>
2024-11-20 12:11:44 +0000

    feat: Added object: calculator and function: reverseString. Also added tests for them.

--- functions.js
+++ functions.js
@@ -2,5 +2,26 @@ function capitalize(string) {
     return string.charAt(0).toUpperCase() + string.slice(1)
 }
 
+function reverseString(string) {
+    return string.split("").reverse().join("")
+}
+
+const calculator = {
+    add(a, b) {
+        return a + b
+    },
+    subtract(a, b) {
+        return a - b
+    },
+    divide(a, b) {
+        return a / b
+    },
+    multiply(a, b) {
+        return a * b
+    },
+}
+
 module.exports = {};
 module.exports.capitalize = capitalize
+module.exports.reverseString = reverseString
+module.exports.calculator = calculator
--- functions.test.js
+++ functions.test.js
@@ -3,3 +3,14 @@ const Functions = require("./functions.js");
 test("String should be capitalized after function is run", () => {
     expect(Functions.capitalize("mary")).toBe("Mary");
 })
+
+test("String should be reverted", () => {
+    expect(Functions.reverseString("James")).toBe("semaJ");
+})
+
+test("Calculator functions should return the right values", () => {
+    expect(Functions.calculator.add(1, 1)).toBe(2);
+    expect(Functions.calculator.subtract(1, 1)).toBe(0);
+    expect(Functions.calculator.divide(2, 1)).toBe(2);
+    expect(Functions.calculator.multiply(2, 1)).toBe(2);
+})
