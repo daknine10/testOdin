b8fd2b6964b81dfdcfc53a9cff939bda11e14d50
daknine10 <156335553+daknine10@users.noreply.github.com>
2024-11-20 14:30:29 +0000

    feat:
    Added ceasarCipher function + testing.

--- functions.js
+++ functions.js
@@ -21,7 +21,37 @@ const calculator = {
     },
 }
 
+function ceasarCipher(string, number) {
+    const abc = 'abcdefghijklmnopqrstuvwxyz'
+    const toCheckLower = abc.split("")
+    const toCheckUpper = abc.toUpperCase().split("")
+    const splitString = string.split("")
+    let output = "";
+
+    for (let letter of splitString) {
+        if (toCheckLower.includes(letter)) {
+            index = toCheckLower.indexOf(letter) + number
+            if (index >= 26) {
+                index = index - 26
+            }
+            output += toCheckLower[index]
+        }
+        else if (toCheckUpper.includes(letter)) {
+            index = toCheckUpper.indexOf(letter) + number
+            if (index >= 26) {
+                index = index - 26
+            }
+            output += toCheckUpper[index]
+        }
+        else {
+            output += letter
+        }
+    }
+    return output
+}
+
 module.exports = {};
 module.exports.capitalize = capitalize
 module.exports.reverseString = reverseString
 module.exports.calculator = calculator
+module.exports.ceasarCipher = ceasarCipher
--- functions.test.js
+++ functions.test.js
@@ -14,3 +14,11 @@ test("Calculator functions should return the right values", () => {
     expect(Functions.calculator.divide(2, 1)).toBe(2);
     expect(Functions.calculator.multiply(2, 1)).toBe(2);
 })
+
+test("Letters should be shifted by the given number", () => {
+    expect(Functions.ceasarCipher("xyz", 3)).toBe("abc")
+    expect(Functions.ceasarCipher("xYz", 3)).toBe("aBc")
+    expect(Functions.ceasarCipher("hello", 3)).toBe("khoor")
+    expect(Functions.ceasarCipher("heLLo", 3)).toBe("khOOr")
+    expect(Functions.ceasarCipher("Hello, World!", 3)).toBe("Khoor, Zruog!")
+})
